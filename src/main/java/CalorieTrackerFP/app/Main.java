8c98d41186d4defd185419e01832e8ba43e46bcd
ColtonG5 <colton.gowans@ucalgary.ca>
2022-04-08 13:32:43 -0600

    Fixed fxml not linked to controller

--- src/main/java/CalorieTrackerFP/app/Main.java
+++ src/main/java/CalorieTrackerFP/app/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Calorie Tracker" + version);
         stage.setScene(scene);
         stage.show();
--- src/main/java/CalorieTrackerFP/app/MainController.java
+++ src/main/java/CalorieTrackerFP/app/MainController.java
@@ -1,7 +1,17 @@
 package CalorieTrackerFP.app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class MainController {
 
-}
+    @FXML
+    private TextArea viewUserInfoTextArea;
 
+    @FXML
+    void viewUserInfoButton(ActionEvent event) {
+        viewUserInfoTextArea.setText("AYO");
+    }
+
+}
