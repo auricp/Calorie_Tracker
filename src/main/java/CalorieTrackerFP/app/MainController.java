631b0b28ebe331532cc630ce73442be60772fd2d
Auric Adubofour-Poku <auric.adubofourpoku@ucalgary.ca>
2022-04-09 15:01:29 -0700

    Added all of the user info functionality as well as error and success messages

--- src/main/java/CalorieTrackerFP/app/MainController.java
+++ src/main/java/CalorieTrackerFP/app/MainController.java
@@ -1,10 +1,122 @@
 package CalorieTrackerFP.app;
 
+import CalorieTrackerFP.person.Person;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainController {
+    @FXML
+    private TextField ageInput;
+
+    @FXML
+    private TextField heightInput;
+
+    @FXML
+    private Label errorMsg;
+
+    @FXML
+    private Label successMsg;
+
+    @FXML
+    private TextField hipInput;
+
+    @FXML
+    private TextField neckInput;
+
+    @FXML
+    private TextField waistInput;
+
+    @FXML
+    private TextField weightInput;
+
+    @FXML
+    private ChoiceBox<String> goalChoose;
+
+    @FXML
+    private ChoiceBox<String> genderChoose;
+
+    Person User = new Person();
+
+    private String[] goals = new String[]{"muscle", "loss", "maintenance"};
+    private String[] genders = new String[]{"man", "woman"};
+
+    @FXML
+    public void initialize(){
+        goalChoose.getItems().addAll(goals);
+        genderChoose.getItems().addAll(genders);
+    }
+
+    @FXML
+    void infoUpdate(MouseEvent event) {
+        User.setGoal(goalChoose.getValue());
+        User.setGender(genderChoose.getValue());
+        try{
+            if(ageInput.getText().equals("")){
+                System.out.print("Wont update age");
+            }else{
+                User.setAge(Integer.parseInt(ageInput.getText()));
+            }
+        }catch(Exception e){
+            System.out.print("Error");
+            errorMsg.setText(ageInput.getText() + "is invalid for age");
+        }
+        try{
+            if(weightInput.getText().equals("")){
+                System.out.print("Wont update weight");
+            }else{
+                User.setWeight(Double.parseDouble(weightInput.getText()));
+            }
+        }catch(Exception e){
+            errorMsg.setText(weightInput.getText() + " is invalid for weight");
+            System.out.print("Error");
+        }
+        try{
+            if(heightInput.getText().equals("")){
+                System.out.print("Wont update height");
+            }else{
+                User.setHeight(Double.parseDouble(heightInput.getText()));
+            }
+        }catch(Exception e){
+            errorMsg.setText(heightInput.getText() + " is invalid for height");
+            System.out.print("Error");
+        }
+        try{
+            if(neckInput.getText().equals("")){
+                System.out.print("Wont update neck measurements");
+            }else{
+                User.setNeckMeasurement(Double.parseDouble(neckInput.getText()));
+            }
+        }catch(Exception e){
+            errorMsg.setText(neckInput.getText() + " is invalid for neck measurement");
+            System.out.print("Error");
+        }
+        try{
+            if(waistInput.getText().equals("")){
+                System.out.print("Wont update waist measurement");
+            }else{
+                User.setWaistMeasurement(Double.parseDouble(waistInput.getText()));
+            }
+        }catch(Exception e){
+            errorMsg.setText(waistInput.getText() + " is invalid for waist measurement");
+            System.out.print("Error");
+        }
+        try{
+            if(hipInput.getText().equals("")){
+                System.out.print("Wont update hip measurement");
+            }else{
+                User.setHipMeasurement(Double.parseDouble(hipInput.getText()));
+            }
+        }catch(Exception e){
+            errorMsg.setText(hipInput.getText() + " is invalid for hip measurement");
+            System.out.print("Error");
+        }
+        successMsg.setText("User info updated!");
+    }
 
     @FXML
     private TextArea viewUserInfoTextArea;
@@ -13,5 +125,4 @@ public class MainController {
     void viewUserInfoButton(ActionEvent event) {
         viewUserInfoTextArea.setText("AYO");
     }
-
 }
--- src/main/java/CalorieTrackerFP/person/Person.java
+++ src/main/java/CalorieTrackerFP/person/Person.java
@@ -1,4 +1,105 @@
 package CalorieTrackerFP.person;
 
 public class Person {
+    private int age;
+    private String goal;
+    private String gender;
+    private double height;
+    private double weight;
+    private double neckMeasurement;
+    private double waistMeasurement;
+    private double hipMeasurement;
+
+    public void setGoal(String goal){
+        //method to ask for changing goal
+        this.goal = goal;
+    }
+
+    public String getGoal(){
+        return goal;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    // setting the gender of the user (either man or woman)
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+    // setting the age of the user in years
+    public void setAge(int AgeYr){
+        age = AgeYr;
+    }
+    // do function to receive the age of the user
+    public int getAge(){
+        return age;
+    }
+    public void setHeight(double height){
+        //do function to ask for new height
+        this.height = height;
+    }
+    public double getHeight(){
+        //do function to get height
+        return height;
+    }
+    public void setWeight(double weight){
+        //do function to ask for new weight. In kg
+        this.weight = weight;
+    }
+    public double getWeight(){
+        return weight;
+    }
+
+    /**
+     * Below are the setters and getters for the Neck, Waist and Hip measurements for the current user. They all work in the same way and return a double of the
+     * selected measurement in cm
+     */
+    public double getNeckMeasurement(){
+        return neckMeasurement;
+    }
+
+    public void setNeckMeasurement(double neckCm){
+        neckMeasurement = neckCm;
+    }
+
+    public double getWaistMeasurement(){
+        return waistMeasurement;
+    }
+
+    public void setWaistMeasurement(double waistCm){
+        waistMeasurement = waistCm;
+    }
+
+    public double getHipMeasurement() {
+        return hipMeasurement;
+    }
+
+    public void setHipMeasurement(double hipMeasurement) {
+        this.hipMeasurement = hipMeasurement;
+    }
+
+    /**
+     * To string method that prints out all of the users measurements. Will only include neck waist and hip measurements if they are entered into the program
+     * @return returns the string form of all of this information to print to the terminal
+     */
+    @Override
+    public String toString() {
+        String output = "";
+        output += "Your measurements: \n";
+        output += "Age: " + age + "\n";
+        output += "Goal: " + goal + "\n";
+        output += "Weight: " + weight + "\n";
+        output += "Height: " + height + "\n";
+        if (neckMeasurement > 0.0) {
+            output += "Neck measurement: " + neckMeasurement + "\n";
+        }
+        if (waistMeasurement > 0.0) {
+            output += "Waist measurement: " + waistMeasurement + "\n";
+        }
+        if (hipMeasurement > 0.0) {
+            output += "Hip measurement: " + hipMeasurement + "\n";
+        }
+        return output;
+    }
+
 }
