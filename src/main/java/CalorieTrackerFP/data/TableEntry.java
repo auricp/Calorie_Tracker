2e9cf1886b26a9d883e912191e3a50ae719c16ec
ColtonG5 <54954541+ColtonG5@users.noreply.github.com>
2022-04-13 22:38:14 -0600

    More comments

--- src/main/java/CalorieTrackerFP/data/TableEntry.java
+++ src/main/java/CalorieTrackerFP/data/TableEntry.java
@@ -1,19 +1,38 @@
 package CalorieTrackerFP.data;
 
+/**
+ * TableEntry is used to put date into the tables. It was easiest to create a new class with just the functionality of
+ * being added to the table so that PropertyValueFactory is easier to use
+ */
 public class TableEntry {
 
+    //fields for names and calories that will be added to the table
     private String name;
     private String calories;
 
+    /**
+     * Stores the name-calorie combo that wants to be added to the table into the fields of this class, which we can
+     * then add to the table
+     * @param name name of item we want to add to the table
+     * @param calories calorie value associated with the name we want to add
+     */
     public TableEntry(String name, String calories) {
         this.name = name;
         this.calories = calories;
     }
 
+    /**
+     * getter for the name field (need these getters to work with PropertyValueFactory, not sure why it's greyed out tbh)
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * getter for the calories' field
+     * @return the calories
+     */
     public String getCalories() {
         return calories;
     }
--- src/main/java/CalorieTrackerFP/data/UserMapData.java
+++ src/main/java/CalorieTrackerFP/data/UserMapData.java
@@ -3,11 +3,27 @@ package CalorieTrackerFP.data;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * UserMapDate is used as an abstract class to specify what we want our maps to be able to do
+ */
 public abstract class UserMapData {
 
+    /**
+     * Maps need to be able to return a copy of that map if we ever need it
+     * @return a copy of the map
+     */
     public abstract HashMap<String,Integer> getMap();
 
+    /**
+     * Maps need to be able to return a nice version of the items and calories so that we can easily work with them
+     * @return two arrayLists that contain an arraylist for the names and the calories
+     */
     public abstract ArrayList<ArrayList<String>> getMapData();
 
+    /**
+     * Maps need to be able to add a new item-calorie combo to them
+     * @param item the item to be added
+     * @param calories the calories of the item
+     */
     public abstract void addToMap(String item, int calories);
 }
